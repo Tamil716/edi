8e8c30c5f0bd1c4bfd505676e87545b94b90e1c3
Sweetsouls <44605547+Sweetsouls@users.noreply.github.com>
2022-10-10 14:53:54 +0530

    python version change

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-db9-26c3#kxkg@dbmkc@o*mgemy10h4lk3@n+faqx6xhtilp*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
